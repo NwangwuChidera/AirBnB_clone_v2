4cbbe557a89d9a38d5a175cd440286858d163302
NwangwuChidera <nwangwupamela@gmail.com>
2023-04-24 01:09:28 -0700

    flasks

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,7 +11,7 @@ app = Flask("__name__")
 @app.route("/", strict_slashes = False)
 def hello_hbnb():
     """return a string"""
-    return ("hello HBNB!")
+    return ("Hello HBNB!")
 
-if __name == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,7 +12,7 @@ app = Flask("__name__")
 @app.route("/", strict_slashes = False)
 def hello_hbnb():
     """return a string"""
-    return ("hello HBNB!")
+    return ("Hello HBNB!")
 
 @app.route("/hbnb", strict_slashes = False)
 def hbnb():
